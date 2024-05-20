e8c32b598e1182fe1938f5fd550dc88a5fc9e1ec
Josh <ahimaz.joshua@gmail.com>
2024-05-20 16:11:32 +0800

    feat: metadata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -11,7 +11,46 @@ type Props = {
 };
 export const metadata: Metadata = {
   title: "Sunway Blockchain Club",
-  description: "The Sunway Blockchain Club is a student-led organization that aims to educate and nurture the next generation into blockchain.",
+  description: "The Sunway Blockchain Club is a Sunway student-led organization that aims to educate and nurture the next generation into blockchain, crypto and Web3 Technologies. Whether they aim to be Developers, Fintech, Finance, Designers or even passionate investors. We guide them to achieve their goals.",
+  applicationName: "Sunway Blockchain Club",
+  keywords: ["Sunway", "Blockchain", "Club", "Crypto", "Web3", "Technology", "Fintech", "Finance", "Designers", "Investors", "Cryptocurrency", "NFT", "Metaverse", "DAO", "DeFi", "Blockchain Club", "Sunway", "Sunway College", "Sunway University"],
+  authors: [{ name: "Sunway Blockchain Club", url: "https://sunwayblockchain.com/"}, { name: "Joshua Ahimaz", url: "https://joshuaahimaz.com/"}],
+  creator: "Joshua Ahimaz",
+  metadataBase: new URL("https://sunwayblockchain.com/"),
+  publisher: "Sunway Blockchain Club",
+  alternates: {
+    canonical: "/",
+    languages: {
+      'en': '/en',
+      'zh': '/zh',
+      'bm': '/bm'
+    }
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+    }
+  },
+  openGraph: {
+    siteName: "Sunway Blockchain Club",
+    url: "https://sunwayblockchain.com/",
+    images: ['/images/logoMETA.png'],
+    type: "website",
+    title: "Sunway Blockchain Club",
+    locale: "en",
+    description: "The Sunway Blockchain Club is a Sunway student-led organization that aims to educate and nurture the next generation into blockchain, crypto and Web3 Technologies. Whether they aim to be Developers, Fintech, Finance, Designers or even passionate investors. We guide them to achieve their goals.",
+  },
+  twitter: {
+    card: "summary",
+    title: "Sunway Blockchain Club",
+    creator: "@SunwayBCC",
+    description: "The Sunway Blockchain Club is a Sunway student-led organization that aims to educate and nurture the next generation into blockchain, crypto and Web3 Technologies. Whether they aim to be Developers, Fintech, Finance, Designers or even passionate investors. We guide them to achieve their goals.",
+    images: ['/images/logoMETA.png'],
+  }
 };
 
 async function getMessages(locale: string) {
