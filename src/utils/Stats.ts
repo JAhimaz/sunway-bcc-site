d55ceba34ffd90b88192393ef1022a77b91ff359
Josh <ahimaz.joshua@gmail.com>
2024-07-04 08:48:56 +0800

    minor changes

--- src/utils/Stats.ts
+++ src/utils/Stats.ts
@@ -1,5 +1,17 @@
 export const Stats = {
-  "members": 200,
-  "events": 20,
-  "cohost": 30,
+  "members": 280,
+  "events": 30,
+  "cohost": 50,
 }
+
+// Sunway Blockchain Club, a society in Sunway University, aims to onboard the next generation of students into the Blockchain / Cryptocurrency industry. With over 280+ members within 6 months, we have grown to be the second biggest blockchain university club in Malaysia. Hosting over 35 events and having both partnerships and collaborations with large scale Malaysia events such as ETHKL, Malaysia Blockchain Week and SOLANA. Are mission to educate has also brought on board major companies to our doorstep to teach our students, such as Coingecko, Etherscan, Solana foundation, Ethereum Foundation, Luno, Blackrock, Aleo, ZKSync, Aptos, NEAR and more.
+
+// We strive to achieve four pillars in our community:
+
+// IGNITE: To ignite the curiosity of the university community, comprising students, researchers, and staff members, about the potential of blockchain technology. The clubs aim to pique interest through engaging discussions, workshops, and interactive sessions.
+
+// NURTURE: To nurture collaborative learning and co-Creation: the club aspires to provide a platform where members can learn from each other, exchange ideas, share knowledge, and jointly explore the multifaceted aspects of blockchain technology.
+
+// EMPOWER: To empower their members with the essential skills and knowledge required to embark on blockchain and cryptocurrency-related careers. By offering educational resources, workshops, and practical experiences, the clubs aim to present blockchain as a viable alternative career path.
+
+// FOSTER: To foster industry relationship with the University community: build connections with key industry leaders to facilitate networking events, seminars, and meetups. Through these interactions, club members can gain firsthand insights into industry trends and potential career avenues.
